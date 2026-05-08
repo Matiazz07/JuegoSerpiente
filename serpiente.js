8ed70c3db0ce457cdea5e9d151439e98a56716ef
Matiazz07 <damian.villota.2007y@gmail.com>
2026-05-07 21:18:16 -0500

    agregar dibujarTablero para crear una cuadricula

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,34 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CANVAS = 500;
+const TAMANIO_CELDA = 25;
 
+function dibujarTablero() {
+  ctx.strokeStyle = "#ff0000ff";
+  ctx.beginPath();
+  for (let i = 0; i <= canvas.width; i += TAMANIO_CELDA) {
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, canvas.height);
+  }
+  for (let i = 0; i <= canvas.height; i += TAMANIO_CELDA) {
+    ctx.moveTo(0, i);
+    ctx.lineTo(canvas.width, i);
+  }
+  ctx.stroke();
+  ctx.lineWidth = 2;
+}
 
-    
+dibujarTodo();
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
-
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
 
 
 
