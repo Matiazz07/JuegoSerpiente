307a21544fdcdc7a740ff17f64ec3970df584276
Matiazz07 <damian.villota.2007y@gmail.com>
2026-05-11 20:20:37 -0500

    agregar numeros a la cuadricula y dibujar cuadrado

--- serpiente.js
+++ serpiente.js
@@ -19,8 +19,6 @@ function dibujarTablero() {
   ctx.lineWidth = 2;
 }
 
-dibujarTodo();
-
 function limpiarCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
@@ -28,7 +26,31 @@ function limpiarCanvas() {
 function dibujarTodo() {
   limpiarCanvas();
   dibujarTablero();
+  dibujarNumeros();
+  pintarCoordenada(23, 23);
 }
 
+function dibujarNumeros() {
+  ctx.fillStyle = "white";
+  ctx.font = "12px Arial";
+  let contador = 0;
+  for (let i = 0; i <= canvas.width; i += TAMANIO_CELDA) {
+    ctx.fillText(contador, 5, i + 12);
+    contador++;
+  }
+  contador = 0;
+  for (let i = 0; i <= canvas.height; i += TAMANIO_CELDA) {
+    ctx.fillText(contador, i + 10, 15);
+    contador++;
+  }
+}
 
-
+function pintarCoordenada(x, y) {
+  if (x < canvas.width && y < canvas.height) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(x * TAMANIO_CELDA, y * TAMANIO_CELDA, TAMANIO_CELDA, TAMANIO_CELDA);
+    ctx.strokeStyle = "blue";
+    ctx.strokeRect(x * TAMANIO_CELDA, y * TAMANIO_CELDA, TAMANIO_CELDA, TAMANIO_CELDA);
+    ctx.lineWidth = 2;
+  }
+}
